0e2d5e0cc9125f345274a447a232eb439758d418
Aylan - Sayzx <74567624+Sayzx@users.noreply.github.com>
2024-10-18 23:38:11 +0200

    Update test_table_testing.lua

--- Basic-Test/test_table_testing.lua
+++ Basic-Test/test_table_testing.lua
@@ -1,8 +1,8 @@
-function run_test(user_code, user_output, expected_output)
+function run_test(test_name, user_code, user_output, expected_output)
     if user_output == expected_output then
-        print("Test Passed!")
+        print(test_name .. ": Test Passed!")
     else
-        print("Test Failed!")
+        print(test_name .. ": Test Failed!")
         print("User Code: \n" .. user_code)
         print("Expected Output: " .. expected_output)
         print("User Output: " .. user_output)
@@ -52,13 +52,13 @@ local user_output = execute_user_code(user_code)
 
 -- Effectuer plusieurs tests
 
--- Test 1 : Vérifier si le tableau a été correctement itéré
+-- Test 1 : Vérifier si la première partie (Max) est correctement imprimée
 local partial_output_test1 = "Name: Max\nAge: 25\nJob: Developer\n"
-run_test(user_code, user_output:sub(1, #partial_output_test1), partial_output_test1)
+run_test("Test 1", user_code, user_output:sub(1, #partial_output_test1), partial_output_test1)
 
--- Test 2 : Vérifier si la seconde personne est correctement affichée
+-- Test 2 : Vérifier si la seconde partie (Jenna) est correctement imprimée
 local partial_output_test2 = "Name: Jenna\nAge: 30\nJob: Designer\n"
-run_test(user_code, user_output:sub(-#partial_output_test2), partial_output_test2)
+run_test("Test 2", user_code, user_output:sub(-#partial_output_test2), partial_output_test2)
 
 -- Test 3 : Vérifier l'output complet
-run_test(user_code, user_output, expected_output)
+run_test("Test 3", user_code, user_output, expected_output)
