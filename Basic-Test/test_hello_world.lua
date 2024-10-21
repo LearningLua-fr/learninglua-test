750a16fac8787040e56dcdfe109dce3a232faa54
Aylan - Sayzx <74567624+Sayzx@users.noreply.github.com>
2024-10-21 17:52:42 +0200

    Update test_hello_world.lua

--- Basic-Test/test_hello_world.lua
+++ Basic-Test/test_hello_world.lua
@@ -1,8 +1,10 @@
 function contains_print_statement(code)
-    return string.match(user_code, "print%(") ~= nil
+    -- Vérifie si le code utilisateur contient un appel à print
+    return string.match(code, "print%(") ~= nil
 end
 
-function final_is_not_equal(code, user_output, expected_output_user)
+function final_is_not_equal(user_output, expected_output_user)
+    -- Vérifie si la sortie utilisateur correspond à la sortie attendue
     if user_output == expected_output_user then
         return true
     else
@@ -10,18 +12,23 @@ function final_is_not_equal(code, user_output, expected_output_user)
     end
 end
 
-function run_test(code)
-    if final_is_not_equal(user_code, user_output, expected_output_user) then
-    print("Test Passed 1/2")
+function run_test(user_code, user_output, expected_output_user)
+    -- Test 1: Vérifie si la sortie utilisateur correspond à la sortie attendue
+    if final_is_not_equal(user_output, expected_output_user) then
+        print("Test Passed 1/2")
     else
-        print("Test Failed! 1 //Output is not equal to expected output"..user_output.." "..expected_output_user)
+        print("Test Failed 1/2: Output is not equal to expected output")
+        print("User Output: [" .. user_output .. "]")
+        print("Expected Output: [" .. expected_output_user .. "]")
     end
 
+    -- Test 2: Vérifie si le code utilisateur contient au moins un `print`
     if contains_print_statement(user_code) then
         print("Test Passed 2/2")
     else
-        print("Test Failed! 2 //No print on code")
+        print("Test Failed 2/2: No print statement found in the code")
     end
 end
 
+-- Appel de la fonction run_test avec les variables user_code, user_output et expected_output_user
 run_test(user_code, user_output, expected_output_user)
