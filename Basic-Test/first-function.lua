df9e62f1db3b727ae4b864d76437b66dd73dd39a
Aylan - Sayzx <74567624+Sayzx@users.noreply.github.com>
2024-10-22 00:38:58 +0200

    Update first-function.lua

--- Basic-Test/first-function.lua
+++ Basic-Test/first-function.lua
@@ -6,38 +6,40 @@ function contains_correct_return_statement(code)
     return string.match(code, "return%s+n%s*%*%s*2") ~= nil
 end
 
-function contains_print_call(code)
-    return string.match(code, "print%s*%(%s*double%s*%(%s*%d+%s*%)%s*%)") ~= nil
+function normalize_string(str)
+    return str:gsub("%s+", " "):gsub("^%s*(.-)%s*$", "%1")
 end
 
-function contains_no_triple(code)
-    return string.match(code, "return%s+n%s*%*%s*3") == nil
+function final_is_not_equal(user_output, expected_output_user)
+    local normalized_user_output = normalize_string(user_output)
+    local normalized_expected_output = normalize_string(expected_output_user)
+    return normalized_user_output == normalized_expected_output
 end
 
-function run_test(user_code)
+function run_test(user_code, user_output, expected_output_user)
+    if final_is_not_equal(user_output, expected_output_user) then
+        print("Test Passed 1/3: Output is correct")
+    else
+        print("Test Failed 1/3: Output is not equal to expected output")
+    end
+
     if contains_function_declaration(user_code) then
-        print("Test Passed 1/3: Function 'double' is correctly defined")
+        print("Test Passed 2/3: Function 'double' is correctly defined")
     else
-        print("Test Failed 1/3: Function 'double' is missing or incorrect")
+        print("Test Failed 2/3: Function 'double' is missing or incorrect")
     end
 
-    if contains_correct_return_statement(user_code) and contains_no_triple(user_code) then
-        print("Test Passed 2/3: Return statement is correct and does not triple")
+    if contains_correct_return_statement(user_code) then
+        print("Test Passed 3/3: Return statement is correct")
     else
-        print("Test Failed 2/3: Return statement is incorrect or triples the value")
+        print("Test Failed 3/3: Return statement is incorrect")
     end
 
-    if contains_print_call(user_code) then
-        print("Test Passed 3/3: Function 'double' is correctly printed")
-    else
-        print("Test Failed 3/3: Function 'double' is not printed or print statement is incorrect")
-    end
-
-    if contains_function_declaration(user_code) and contains_correct_return_statement(user_code) and contains_print_call(user_code) and contains_no_triple(user_code) then
+    if final_is_not_equal(user_output, expected_output_user) and contains_function_declaration(user_code) and contains_correct_return_statement(user_code) then
         print("All tests passed")
     else
         print("Some tests failed")
     end
 end
 
-run_test(user_code)
+run_test(user_code, user_output, expected_output_user)
