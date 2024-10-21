ac4ed5bfd18c572c43b898a6dad9f9f436de24b8
Aylan - Sayzx <74567624+Sayzx@users.noreply.github.com>
2024-10-21 17:29:31 +0200

    Update test_hello_world.lua

--- Basic-Test/test_hello_world.lua
+++ Basic-Test/test_hello_world.lua
@@ -1,11 +1,9 @@
+
 function contains_print_statement(code)
-    return string.match(code, "print%(") ~= nil
+    return string.match(user_code, "print%(") ~= nil
 end
 
-local passed = 0
-local failed = 0
-
-function final_is_not_equal(user_output, expected_output)
+function final_is_not_equal(code, user_output, expected_output)
     if user_output == expected_output then
         return true
     else
@@ -13,33 +11,18 @@ function final_is_not_equal(user_output, expected_output)
     end
 end
 
-function run_test(user_code, user_output, expected_output)
-    -- Test 1: Vérifier si la sortie est égale à la sortie attendue
-    if final_is_not_equal(user_output, expected_output) then
+function run_test(code)
+    if final_is_not_equal(user_code, user_output, expected_output) then
         print("Test Passed 1/2")
-        passed = passed + 1
     else
-        print("Test Failed 0/2 // Output is not equal to expected output")
-        failed = failed + 1
+        print("Test Failed! 1 //Output is not equal to expected output")
     end
 
-    -- Test 2: Vérifier s'il y a au moins un `print` dans le code utilisateur
     if contains_print_statement(user_code) then
         print("Test Passed 2/2")
-        passed = passed + 1
     else
-        print("Test Failed 1/2 // No print statement in code")
-        failed = failed + 1
+        print("Test Failed! 2 //No print on code")
     end
-
-    -- Afficher le récapitulatif des tests
-    print_test_summary()
 end
 
--- Fonction pour afficher le récapitulatif final
-function print_test_summary()
-    print("Test Summary: " .. passed .. " passed, " .. failed .. " failed.")
-end
-
--- Appeler la fonction run_test avec les variables user_code, user_output, expected_output
-run_test(user_code, user_output, expected_output)
+run_test(user_code)
