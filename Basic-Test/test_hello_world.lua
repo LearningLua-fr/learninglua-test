94a909611818ba770eafc5ae2584a8975af32010
Aylan - Sayzx <74567624+Sayzx@users.noreply.github.com>
2024-10-21 17:23:21 +0200

    Update test_hello_world.lua

--- Basic-Test/test_hello_world.lua
+++ Basic-Test/test_hello_world.lua
@@ -3,11 +3,25 @@ function contains_print_statement(code)
     return string.match(user_code, "print%(") ~= nil
 end
 
-function run_test(code)
-    if contains_print_statement(user_code) then
-        print("Test Passed 1/1")
+function final_is_not_equal(code, user_output, expected_output)
+    if user_output == expected_output then
+        return true
     else
-        print("Test Failed! 1/1 //No print on code")
+        return false
+    end
+end
+
+function run_test(code)
+    if final_is_not_equal(user_code, user_output, expected_output) then
+        print("Test Passed 1/2")
+    else
+        print("Test Failed! 0/2 //Output is not equal to expected output")
+    end
+
+    if contains_print_statement(user_code) then
+        print("Test Passed 2/2")
+    else
+        print("Test Failed! 1/2 //No print on code")
     end
 end
 
