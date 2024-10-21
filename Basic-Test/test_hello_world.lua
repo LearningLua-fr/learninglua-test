c0618725c30d8de06526cf334ce30b6efc9d3577
Aylan - Sayzx <74567624+Sayzx@users.noreply.github.com>
2024-10-21 17:10:14 +0200

    Update test_hello_world.lua

--- Basic-Test/test_hello_world.lua
+++ Basic-Test/test_hello_world.lua
@@ -1,3 +1,14 @@
-print("le code")
-print(user_code)
 
+function contains_print_statement(code)
+    return string.match(user_code, "print%(") ~= nil
+end
+
+function run_test(code)
+    if contains_print_statement(user_code) then
+        print("Test Passed 1/1")
+    else
+        print("Test Failed! 1/1")
+    end
+end
+
+run_test(user_code)
